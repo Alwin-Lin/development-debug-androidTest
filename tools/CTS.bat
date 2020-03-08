b2dfb151b9920db60db885fa8133b46194aca729
Alwin-Lin <alwin001@gmail.com>
2020-03-08 14:43:37 -0700

    Delete CTS.bat

--- tools/CTS.bat
+++
@@ -1,8 +0,0 @@
-set SDK_ROOT=C:\Users\alwin\AppData\Local\Android\Sdk
-set CTS_ROOT=D:\cts
-set JAR_DIR=%CTS_ROOT%\android-cts\tools
-set JAR_PATH=%JAR_DIR%\tradefed.jar;%JAR_DIR%\loganalysis.jar;%JAR_DIR%\hosttestlib.jar;%JAR_DIR%\compatibility-common-util-tests.jar;%JAR_DIR%\compatibility-common-util-tests.jar;%JAR_DIR%\cts-tradefed.jar;%JAR_DIR%\cts-tradefed-tests.jar;%JAR_DIR%\compatibility-host-util.jar;%JAR_DIR%\compatibility-tradefed-tests.jar;%JAR_DIR%\host-libprotobuf-java-full.jar
-echo  java -Xmx1024M -XX:+HeapDumpOnOutOfMemoryError -cp %JAR_PATH% -DCTS_ROOT=%CTS_ROOT% com.android.compatibility.common.tradefed.command.CompatibilityConsole 
-
- java -Xmx1024M -XX:+HeapDumpOnOutOfMemoryError -cp %JAR_PATH% -DCTS_ROOT=%CTS_ROOT% com.android.compatibility.common.tradefed.command.CompatibilityConsole
-
