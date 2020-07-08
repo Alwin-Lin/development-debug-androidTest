e45fc20ccee7cdda7dd64e5c5fd340c988d0c535
Alwin-Lin <alwin001@gmail.com>
2020-07-08 10:48:58 -0700

    Refactor NewProjectDialogBox, add in default paths

--- android_sys_app_plugin/src/main/java/MyAction.java
+++ android_sys_app_plugin/src/main/java/MyAction.java
@@ -8,7 +8,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 
-import javax.swing.JButton;
+import java.io.File;
 
 public class MyAction extends AnAction {
     private final NotificationGroup NOTIFICATION_GROUP = new NotificationGroup("android_sys_app_plugin", NotificationDisplayType.BALLOON, true);
@@ -27,11 +27,16 @@ public class MyAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         notify(e.getProject(), "todo Action");
-        com.alwin.asap.DialogBox dlg = new com.alwin.asap.DialogBox();
+        com.alwin.asap.NewProjectDialogBox dlg = new com.alwin.asap.NewProjectDialogBox("C:\\Users\\alwin\\ws\\20200708\\development-debug-androidTest\\CtsCarTestCases\\ctsSource", "C:\\Users\\alwin\\ws\\20200708\\development-debug-androidTest\\android_sys_app_plugin\\testing");
         dlg.show();
         if (dlg.isOK()){
-            String outputPath = dlg.getOutputPath();
-            String appSourcePath = dlg.getAppSourcePath();
+            String outputPath = dlg.getOutputPath().replaceAll("\\\\", "/");
+            String appSourcePath = dlg.getAppSourcePath().replaceAll("\\\\", "/");
+            // Create output directory if not exist
+            File outputDir = new File(outputPath);
+            if (! outputDir.exists()){
+                outputDir.mkdirs();
+            }
             com.alwin.asap.GradleFileGenerator gradleFileGenerator = new com.alwin.asap.GradleFileGenerator(appSourcePath, outputPath, "build.gradle");
             gradleFileGenerator.createBuildGradleFile();
         }
